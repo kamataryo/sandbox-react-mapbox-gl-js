cd77f21675e69a1057bcf7ec2f47222e532e4376
kamataryo <mugil.cephalus+ciconia.local@gmail.com>
2018-12-04 12:39:55 +0900

    Display map

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import mapboxgl from "mapbox-gl";
+
+mapboxgl.accessToken =
+  "pk.eyJ1Ijoia2FtYXRhcnlvIiwiYSI6ImNqbGF1YTJ6czA4a2szcGs0aTZkZHFvMHcifQ.ibLjeMOG6KfbxP4YV0ViSg";
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
